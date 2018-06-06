95a696fc2f14f87c3ba69858932d1da66e5cc926
MehSwitch <aymehaiDev@protonmail.com>
2018-06-06 14:39:41 -0400

    Added a Button and Textview that when the button is clicked the Textview changes

--- app/src/main/java/com/example/aymen/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/aymen/myapplication/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.aymen.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        changeText()
+
+    }
+
+    fun changeText(){
+        btnSayHello.setOnClickListener {
+            lblText.setText("Hello")
+        }
     }
 }
